28741124b195e1bd1a4f95d66a7f8cf8db4087b0
Shivateja Burle <shivateja.idizital@gmail.com>
2025-12-08 18:32:36 +0530

    Customize Task admin list display
    Introduced AdminTask class to show 'title' and 'completed' fields in the Django admin list view for Task model.

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
-admin.site.register(Task)
+class AdminTask(admin.ModelAdmin):
+    list_display = ('title', 'completed')
+
+admin.site.register(Task, AdminTask)
