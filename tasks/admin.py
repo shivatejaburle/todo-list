aa1d99716e2099eb741c6dcc9590becd47c49382
Shivateja Burle <shivateja.idizital@gmail.com>
2025-12-11 18:58:51 +0530

    Revert "Customize Task admin list display"
    This reverts commit 28741124b195e1bd1a4f95d66a7f8cf8db4087b0.

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import Task
 
 # Register your models here.
-class AdminTask(admin.ModelAdmin):
-    list_display = ('title', 'completed')
-
-admin.site.register(Task, AdminTask)
+admin.site.register(Task)
